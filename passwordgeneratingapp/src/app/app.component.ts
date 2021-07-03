431309adcb6ae66974ad06047dfd9ddbfacb6818
pratyushvatsalshukla <pratyushshukla9333@gmail.com>
2021-07-03 18:48:00 +0530

    App Working !!
    Added All the Functionalities and now i have to style it.

--- passwordgeneratingapp/src/app/app.component.ts
+++ passwordgeneratingapp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { generate } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  length = 0 ;
   includeLetters = false ;
   includeNumbers = false ;
   includeSymbols = false ;
   password = '' ;
 
+  onChangeLength(event:any){
+   const parsedValue = parseInt(event.target.value) ;
+   if(!isNaN(parsedValue))
+   {
+      this.length = parsedValue ;
+   }
+   
+  }
   
   onChangeUseLetters(){
     this.includeLetters = !this.includeLetters ;
@@ -25,18 +35,37 @@ export class AppComponent {
   }
 
   onButtonClick(){
-    console.log(this.includeLetters) ;
-    console.log(this.includeNumbers) ;
-    console.log(this.includeSymbols) ;
-    this.password = "My Password" ;
+    const numbers = '1234567890' ;
+    const letters = 'qwertyuioplkjhgfdsazxcvbnm' ;
+    const symbols = '!@#$%^&*(){}":?><,./;[]' ;
+
+    let validChars = '' ;
+    if(this.includeLetters){
+      validChars += letters ;
+    }
+    if(this.includeNumbers){
+      validChars += numbers ;
+    }
+    if(this.includeSymbols){
+      validChars += symbols ;
+    }
+
+    let generatedPassword ='' ;
+    for(let i = 0 ; i < this.length ; i++){
+      const index = Math.floor(Math.random() * validChars.length) ;
+      generatedPassword += validChars[index] ;
+    }
+
+    this.password = generatedPassword ;
+
   }
 
   // getPassword(){
   //   return this.password ;
   // }
 
-  getName(){
-    return "Pratyush" ;
+  getPassword(){
+    return this.password;
   }
 
 }
